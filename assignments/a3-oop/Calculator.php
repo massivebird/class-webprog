cc83ff3f9aa6b23c17fffff05c4cd0dcd8bb56b3
massivebird <gdrakemail@gmail.com>
2022-10-04 21:32:34 -0400

    + a3 completed

---
+++ assignments/a3-oop/Calculator.php
@@ -0,0 +1,41 @@
+<?php
+class Calculator {
+
+   // print formatted error 
+   private function printError($message) {
+      echo "Error: Calculator class: ".$message."<br>";
+   }
+
+   public function calc($operator=Null, $num1=Null, $num2=Null) {
+      // verify correct number of arguments
+      if (is_null($operator) || is_null($num1) || is_null($num2)) {
+	 self::printError("you need a string and two operators");
+	 return;
+      }
+      // verify valid operator
+      if (substr_count("+-/*", $operator) != 1) {
+	 self::printError("Invalid operator");
+	 return;
+      }
+
+      // calculations
+      if ($operator == "+") {
+	 echo "The sum of the numbers is ".($num1+$num2)."<br>";
+      }
+      else if ($operator == "-") {
+	 echo "The difference of the numbers is ".($num1-$num2)."<br>";
+      }
+      else if ($operator == "*") {
+	 echo "The product of the numbers is ".($num1*$num2)."<br>";
+      }
+      else if ($operator == "/") {
+	 if ($num2 == 0) {
+	    self::printError("Cannot divide by zero");
+	    return;
+	 }
+	 echo "The division of the numbers is ".($num1/$num2)."<br>";
+      } 
+   }
+}
+
+?>
