28a4f08d93b0471445e36baf97fd9df96d6c5e91
massivebird <gdrakemail@gmail.com>
2022-10-11 19:51:53 -0400

    + sandbox work

--- sandbox/index.php
+++ sandbox/index.php
@@ -1,47 +1,38 @@
 <?php
 
-class Account {
+$output = "";
 
-   private $_totalMoney;
-
-   public function __construct($amount) {
-      $this->_totalMoney = $amount;
-      echo "You now have an account with $" . $this->_totalMoney . ".<br>";
-   }
-
-   public function deposit($amount) {
-      $this->_totalMoney += $amount;
-   }
-
-   public function withdraw($amount) {
-      if ($amount <= $this->_totalMoney) {
-	 $this->_totalMoney -= $amount;
-	 echo "You withdrew $" . $amount . ", which means...<br>";
-	 self::viewMoney();
-      } else {
-	 echo "You're broke lmao" . "<br>";
-      }
-   }
-
-   public function viewMoney() {
-      echo "You have $" . $this->_totalMoney . ".<br>";
-   }
-
+if (isset($_POST['submitAndPrint']) and !empty($_POST['firstName'])) {
+   $output = <<<HTML
+   <p>Thanks for submitting the form,
+   {$_POST['firstName']}!</p>
+   HTML;
 }
-
-echo "<pre>";
-$myAccount = new Account(500);
-$myAccount->deposit(500);
-$myAccount->viewMoney();
-$myAccount->withdraw(600);
-
 ?>
 <!DOCTYPE html>
 <html lang="en">
-   <head>
-      <meta charset="UTF-8">
-      <title>Garrett's Sandbox</title>
-   </head>
-   <body>
-   </body>
+<head>
+   <meta charset="UTF-8">
+   <title>Garrett's Sandbox</title>
+</head>
+<style>
+* {
+   font-family: "Helvetica";
+}
+</style>
+<body>
+<h1>The form</h1>
+<?php echo $output; // print_r($_POST['submitAndPrint']); ?>
+<form action="index.php" method="post">
+   <input type="text" name="firstName" id="firstName" placeholder="Enter your name here" />
+   <br><br>
+   <label> Show name 
+      <input type="checkbox" name="submitAndPrint" id="submitAndPrint" value="submit" checked />
+   </label>
+   <br><br>
+   <input type="submit" name="submitButton" id="submitButton" value="Submit form" />
+</form>
+<br>
+<a href="index.php">Reset</a>
+</body>
 </html>
